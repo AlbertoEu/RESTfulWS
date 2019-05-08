491b7b42792f307aa4f266771f5ebb88d520b5f8
AlbertoReyes <a.e.reyes@hotmail.com>
2019-05-08 00:41:56 -0500

    User v1.0

--- src/main/java/me/jmll/utm/model/User.java
+++ src/main/java/me/jmll/utm/model/User.java
@@ -10,6 +10,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement
 public class User implements Serializable{
+	
 	private static final long serialVersionUID = 1L;
 	private String username;
 	private String password;
